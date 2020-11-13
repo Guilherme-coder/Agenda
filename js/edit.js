b269bf0f7676aad16cd3a84f0e99f7e16936924f
Guilherme Hendres <ghendessmelo@gmail.com>
2020-11-13 11:54:20 -0300

    Bug na instrução de editar dados

--- js/edit.js
+++ js/edit.js
@@ -7,13 +7,30 @@ table.addEventListener("click", (event) => {
         const newName = changes[0];
         const newNumber = changes[1];
         const newEmail = changes[2];
+        const newData = new Array({name: newName, number: newNumber, email: newEmail});
         editTable(newName, newNumber, newEmail);
         event.target.parentNode.remove();
-    }
+        let excluded = event.target.parentNode;
+        let nome = excluded.childNodes;
+        for(let i = 0 ; i < localStorage.length ; i++){
+            let data = JSON.parse(localStorage.getItem(i));
+            if(data[0].name === nome[0].textContent){    
+                localStorage.removeItem(i);
+                let fakeData = new Array();
+                fakeData.push({name: "0", number: "0", email: "0",});
+                localStorage.setItem(i, JSON.stringify(fakeData));
+                localStorage.setItem(i, JSON.stringify(newData));
+            }
+            else{
+                console.log("Algo deu errado.");
+            };
+
+        };
+    };
+    setEmpty(input);
 });
 
 
 function editTable(name, number, email){
     makeTr(name, number, email);
-
-}
+};
--- js/saver.js
+++ js/saver.js
@@ -19,23 +19,3 @@ function searchPosition(){
     };
     return i;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-// var people = new Array();
-// if (localStorage.hasOwnProperty("people")) {
-//     people = JSON.parse(localStorage.getItem("people"))
-// };
-// people.push({name: nameTd, number: numberTd, email: emailTd});
-// localStorage.setItem("people", JSON.stringify(people));
-// console.log(people[0]);
-// console.log(people[0].name);
